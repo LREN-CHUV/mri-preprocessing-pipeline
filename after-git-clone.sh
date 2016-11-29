7304142b36c81c64864779bae9abe77440c1cea5
Ludovic Claude <ludovic.claude54@gmail.com>
2016-11-29 14:54:15 +0000

    Comment

--- after-git-clone.sh
+++ after-git-clone.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# Authorised users should sign their commit with PGP to allow the deployment of
+# the code to production
+
 if [ "$USER" = "ludovic" ]; then
   git config commit.gpgsign true
 fi
