6ee790bdda375782dd3d77d400f23ebcb0e9e187
Ludovic Claude <ludovic.claude54@gmail.com>
2017-01-17 09:58:56 +0100

    Remove old code

--- Pipelines/Old_versions/DiffusionPipelinePackage_v2/Automation/run_DWI_pipeline.m
+++
@@ -1,5 +0,0 @@
-DataFolder = 'D:\PipelineTesting\RawData\';
-OutputFolder = 'D:\PipelineTesting\Test\';
-ProtocolsFile = 'C:\Users\dslater\Documents\MATLAB\Software_packages\Pipeline_v2\LestersFunctions\Protocols_definition.txt';
-SubjectID = 'PR00197';
-DWI_pipeline(DataFolder,OutputFolder,ProtocolsFile,SubjectID);
